687f9c58c6243cd521c2db769850886506f01f9e
Rogério <ethicalhacker.dev@gmail.com>
2024-05-08 21:23:03 +0200

    Add project files.

---
+++ Bank/Entities/Account.cs
@@ -0,0 +1,45 @@
+﻿using Bank.Entities.Excepitions;
+using System.Globalization;
+
+
+namespace Bank.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount; 
+        }
+        public void Withdraw(double amount)
+        {
+            if(Balance == 0.0)
+            {
+                throw new DomaineExceptions("You don't have money in your account"); 
+            }
+            if(amount > WithdrawLimit)
+            {
+                throw new DomaineExceptions("The amount exceeds withdraw limit"); 
+            }
+            if (amount > Balance)
+            {
+                throw new DomaineExceptions("Not enough balance "); 
+            }
+            
+            Balance -= amount; 
+        }
+
+    }
+}
---
+++ Bank/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using Bank.Entities;
+using Bank.Entities.Excepitions;
+
+namespace bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            List<Account> list = new List<Account>();
+
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CI);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CI);
+                //Instanciando as variaveis na lista Account
+                list.Add(new Account(number, holder, balance, withdrawLimit));
+                //Operações de saques
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CI);
+
+                foreach (Account acc in list)
+                {
+                    acc.Withdraw(amount);
+                    Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CI));
+                }
+            }
+            catch (DomaineExceptions e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in Format!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+        }
+    }
+}
